bb20c565a265011ea9f7126b6b6688eab57c3315
jackyu0915 <jackyu0915@gmail.com>
2020-06-16 21:16:14 +0800

    change some ui

--- timetable/views.py
+++ timetable/views.py
@@ -3,61 +3,19 @@ from timetable import models
 from functools import wraps
 import os
 from platform import system as system_platform
-import time
 import logging
 import argparse
 from getpass import getpass
 from datetime import datetime, timedelta
 from pytz import timezone
 from timetable.getClassSchedule import *
-from aip import AipOcr
+
 from PIL import Image
 import json
+import time
 
 
-# Create your views here.
-
-
-# 文字识别
-def get_file_content(file):
-    with open(file, 'rb') as fp:
-        return fp.read()
-
-
-def img_to_str(image_path):
-    # 百度文字精确识别设置
-    config = {
-        'appId': '19099446',
-        'apiKey': 'CaKSFYkYzCl98K6hVxT1ji2q',
-        'secretKey': 'cgW37s22yBcXVVCDFbA99pxy7gE8h9Pl'
-    }
-    client = AipOcr(**config)
-    image = get_file_content(image_path)
-    options = {}
-    options["language_type"] = "ENG"
-    result = client.basicAccurate(image, options)
-    # result = client.basicGeneral(image, options)
-    if 'words_result' in result:
-        return '\n'.join([w['words'] for w in result['words_result']])
-
-
-def img_to_str1(image_path):
-    # 百度文字基本识别设置
-    config = {
-        'appId': '19099446',
-        'apiKey': 'CaKSFYkYzCl98K6hVxT1ji2q',
-        'secretKey': 'cgW37s22yBcXVVCDFbA99pxy7gE8h9Pl'
-    }
-    client = AipOcr(**config)
-    image = get_file_content(image_path)
-    options = {}
-    # 设置语言为英文
-    options["language_type"] = "ENG"
-    result = client.basicGeneral(image, options)
-    if 'words_result' in result:
-        return '\n'.join([w['words'] for w in result['words_result']])
-
-
+# 登录
 def aao_logins(request):
     # 学号及密码
     error = ''
@@ -69,8 +27,9 @@ def aao_logins(request):
         userInfo = models.user_info.objects.get(user=stuID)
         logintimes = int(userInfo.times)
         print('------------')
+        print("登陆次数：")
         print(logintimes)
-        if userInfo != '':
+        if userInfo:
             if userInfo.pwd == stuPwd:
                 # 记录登陆次数
                 logintimes = logintimes + 1
@@ -97,44 +56,30 @@ def aao_logins(request):
                 semester_year = '2019-2020'
                 semester = '2'
                 semester_start_date = datetime(2020, 3, 16, 0, 0, 0, tzinfo=timezone('Asia/Shanghai'))
-
-                # 百度文字识别接口识别，每天免费500次，这里调用了两次，防止不一样
-                try:
-                    captcha_str = ''
-                    while True:
-                        # Captcha 验证码 # Fix Issue #13 bug, but only for Windows & MacOS.
-                        captcha_resp = session.get(host + '/eams/captcha/image.action')  # Captcha 验证码图片
-                        captcha_img = Image.open(BytesIO(captcha_resp.content))
-                        # captcha_img.show()  # show the captcha
-                        cap = captcha_img.resize((95, 35), Image.ANTIALIAS)
-                        cap.save("cap0417.png")
-                        captcha_str = img_to_str('cap0417.png')
-                        captcha_str = captcha_str.replace(' ', '')
-                        # 由于使用了精确文字识别，经常只显示三个字母
-                        if len(captcha_str) > 3:
-                            break
-                    print(captcha_str)
+                captcha_str = ''
+                err = 1
+                while (err):
+                    err += 1
+                    if err == 5:
+                        error = "系统错误，请重试"
+                        return render(request, 'aao_login.html', {'error': error})
                     name = aao_login(stuID, stuPwd, captcha_str, retry_cnt)
-                    temp_time = time.time()  # 计个时看看
-                    print('\n## Meow~下面开始获取{}课表啦！\n'.format({0: '个人', 1: '班级'}.get(choice)))
-                    courseTable = getCourseTable(choice=choice)
 
-                    list_lessonObj = parseCourseTable(courseTable)
-                    for lesson in list_lessonObj:
-                        models.lesson_info.objects.create(info=lesson, user=stuID)
-                    print('## 下面开始获取考试信息啦！\n')
-                    examSchedule = getExamSchedule()
-                    list_examObj = parseExamSchedule(examSchedule)
-                except Exception as e:
-                    print("ERROR! 欢迎在GitHub上提出issue & Pull Request!")
-                    print(e)
-                finally:
-                    session.cookies.clear()  # 清一下cookie
-                    if system_platform() == 'Windows':  # Fix Linux `sh: 1: pause: not found` bug
-                        os.system('pause')
+                    if name != 'error':
+                        err = 0
+                temp_time = time.time()  # 计个时看看
+                print('\n## Meow~下面开始获取{}课表啦！\n'.format({0: '个人', 1: '班级'}.get(choice)))
+                courseTable = getCourseTable(choice=choice)
+
+                list_lessonObj = parseCourseTable(courseTable)
+                for lesson in list_lessonObj:
+                    models.lesson_info.objects.create(info=lesson, user=stuID)
+                session.cookies.clear()
                 return ret
             else:
-                error = '用户名或者密码错误'
+                error = "密码错误"
+        else:
+            error = "对不起，请联系作者开放登录权限"
     return render(request, 'aao_login.html', {'error': error})
 
 
@@ -168,7 +113,7 @@ def home(request):
     infos = []
     for i in infos1:
         infos.append(i)
-    # print(infos)
+    print(infos)
     # 处理数据
     les_info = []
     les_info1 = []
@@ -178,8 +123,7 @@ def home(request):
             les_info1 = str(info[1]).split('|')
             # print(les_info1)
             les_info.append(les_info1)
-    print('上课信息获取成功')
-    # print(les_info)
+    print(les_info)
     # 生成二维列表
     list_lesson = [['' for i in range(11)] for j in range(7)]
     for list in les_info:
@@ -188,7 +132,7 @@ def home(request):
         weeks = list[3]
         weeks = list[3][:3] + '-' + list[3][-3:]
         weeks = weeks.replace(',', '')
-        # print(weeks)
+        print(weeks)
         a = str(list[4])
         times = a
         times = times.replace('星期', '')
